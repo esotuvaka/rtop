c24433afc381cdb92cc4e7d700466221380c7d64
esotuvaka <eric.otuvaka@gmail.com>
2025-06-08 16:59:11 -0700

    feat: add basic info on CPUs as servable requests

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
-use axum::{routing::get, Router};
+use std::sync::{Arc, Mutex};
+use sysinfo::System;
+use axum::{routing::get, Router, extract::State};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root_get));
+    let app = Router::new()
+        .route("/", get(root_get))
+        .with_state(AppState { sys: Arc::new(Mutex::new(System::new())) });
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap()
 }
 
-async fn root_get() -> &'static str {
-    "hello, world!"
+#[derive(Clone)]
+struct AppState {
+    sys: Arc<Mutex<System>>,
+}
+
+async fn root_get(State(state): State<AppState>) -> String {
+    use std::fmt::Write;
+
+    let mut s = String::new();
+    let mut sys = state.sys.lock().unwrap();
+    sys.refresh_cpu_usage();
+    for (i, cpu) in sys.cpus().iter().enumerate() {
+        let i = i + 1;
+
+        let usage = cpu.cpu_usage();
+        writeln!(&mut s, "CPU {i} {usage}%").unwrap(); 
+    }
+
+    s
 }
